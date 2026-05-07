f84518459697d0cd3db0a471c6e1003b2c0d1d46
Sharma <niharika.h.sharma@accenture.com>
2026-05-07 12:33:23 +0530

    Setup App for Vercel

--- public/js/app.js
+++ public/js/app.js
@@ -30,7 +30,7 @@ weatherForm.addEventListener('submit',(e)=>{
     messageTwo.textContent = ''
 
     // console.log(location)
-    fetch('http://localhost:3000/weather/?address='+location).then((response) =>{
+    fetch('/weather/?address='+location).then((response) =>{
     response.json().then((data)=>{
         // console.log(data)
         if(data.error){
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { title } = require('process')
 const { error } = require('console')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const geocode = require('./utils/geocode')
 const forecast=require('./utils/forecast')
@@ -164,6 +165,6 @@ app.use((req, res) => {
     })
 })
 
-app.listen(3000, () =>{
-    console.log('Server is up on port 3000.')
+app.listen(port, () =>{
+    console.log('Server is up on port '+port)
 })
